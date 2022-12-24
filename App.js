8d1bb91066a0a12a31e5623b16f6f3f387f930f8
William Leung <gwilliamleung@gmail.com>
2022-12-24 17:57:11 -0500

    added modal, cases for null inputs and  task rolls

--- App.js
+++ App.js
@@ -1,15 +1,33 @@
 import React, { useState } from 'react';
-import { KeyboardAvodingView, StyleSheet, Text, View, SafeAreaView, Button, Alert, Platform, TextInput, TouchableOpacity, Keyboard } from 'react-native';
+import { KeyboardAvodingView, StyleSheet, Text, View, SafeAreaView, Modal, Alert, Platform, TextInput, TouchableOpacity, Keyboard, Image, KeyboardAvoidingView } from 'react-native';
 import Task from './components/Task';
 
 export default function App() {
   const [task, setTask] = useState();
   const [taskItems, setTaskItems] = useState([]);
+  const [source, setSource] = useState(require('./assets/gachastill.png'))
+  const [ModalVisibility, setModalVisibility] = useState(false);
+  const [selectedTask, setSelectedTask] = useState();
 
   const handleAddTask = () => {
     Keyboard.dismiss();
-    setTaskItems([...taskItems, task])
-    setTask(null)
+    if (task && task.trim() !== '') {
+      setTaskItems([...taskItems, task])
+      setTask(null)
+    }
+  }
+
+  const getTask = () => {
+    if (taskItems.length !== 0){
+      console.log(taskItems)
+      setSource(require('./assets/gacha.gif'))
+      setTimeout(() => {
+        setSource(require('./assets/gachastill.png'))
+        pressModal();
+      }, 2250);  
+    } else {
+      alert("No task available, please write some");
+    }
   }
 
   const completeTask = (index) => {
@@ -17,12 +35,44 @@ export default function App() {
     itemsCopy.splice(index, 1);
     setTaskItems(itemsCopy);
   }
+
+  const pressModal = () => {
+    let randomTask = Math.floor(Math.random()*taskItems.length)
+    setSelectedTask(taskItems[randomTask])
+    setModalVisibility(!ModalVisibility)
+    if(ModalVisibility){
+      completeTask(randomTask);
+    }
+  }
   return (
     <SafeAreaView style={styles.container}>
+    <Modal
+      transparent={true}
+      animationType='fade'
+      visible={ModalVisibility}
+      nRequestClose={() => {
+        setModalVisibility(!ModalVisibility);
+      }}
+    >
+    <View style={styles.modalContainer}>
+      <TouchableOpacity >
+        <Text style={styles.modalText}>{selectedTask}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity >
+        <Text style={styles.modalText} onPress={() => pressModal()}>{"close"}</Text>
+      </TouchableOpacity>
+    </View>
+    </Modal>
       <View>
-        <Text style={{fontWeight: 'bold',fontSize:48}}>WHEEL</Text>
-        <View style={{fontSize:24}}>
-            {
+        <TouchableOpacity onPress={() => getTask()}>
+          <Image 
+            source={source}
+          />
+        </TouchableOpacity>
+      </View>   
+      <Text style={{fontWeight: 'bold',fontSize:32}}>Roll your task!</Text>
+      <View style={styles.taskWrapper}>
+      {
               taskItems.map((item, index) => {
                 return (
                   <TouchableOpacity key={index} onPress={() => completeTask(index)}>
@@ -31,12 +81,18 @@ export default function App() {
                 )
               })
             }
-        </View>
-      </View> 
-      <TextInput placeholder={'Test'} value={task} onChangeText={text => setTask(text)}/>
-      <TouchableOpacity onPress={() => handleAddTask()}>
-        <Text>+</Text>
-      </TouchableOpacity>
+      </View>      
+      <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.writeTaskWrapper}
+     > 
+        <TextInput style={styles.input} placeholder={'Write a task...'} value={task} onChangeText={text => setTask(text)}/>
+          <TouchableOpacity onPress={() => handleAddTask()}>
+            <View style={styles.addWrapper}>
+              <Text style={styles.addText}>+</Text>
+            </View>
+          </TouchableOpacity>  
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
@@ -44,8 +100,59 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F5F5DC',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  taskWrapper: {
+    width:'100%',
+    justifyContent:'center',
+    paddingLeft: 60,
+
+  },
+  writeTaskWrapper:{
+    width:'100%',
+    position: 'absolute',
+    bottom:60,
+    flexDirection:'row',
+    justifyContent:'space-around',
+    padding:20
+  },
+  input: {
+    paddingHorizontal: 30,
+    paddingVertical: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    width:250,
+  },
+  addWrapper: {
+    width: 60,
+    height: 60,
+    borderRadius: 60,
+    backgroundColor: '#FFF',
+    justifyContent: 'center',
+    alignItems:'center',
+  },
+  modalContainer: {
+    top:150,
+    bottom:150,
+    left:50,
+    right:50,
+    position: 'absolute',
+    backgroundColor: '#ebdac7',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 60,
+    borderColor: '#c5baaf',
+    borderWidth:'1',
+  },
+  modalText:{
+    fontSize:32,
+    color:'#424b54',
+    marginTop:115,
+  },
+  modalButton:{
+    top:125,
+    fontSize:32,
+    width:300,
+  }
 });
--- components/Task.js
+++ components/Task.js
@@ -1,11 +1,23 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 
 const Task = (props) => {
     return (
-        <View >
-            <Text>{props.text}</Text>
+        <View style={styles.item}>
+            <View style={styles.itemLeft}>  
+                <TouchableOpacity style={styles.square}></TouchableOpacity>
+                <Text>{props.text}</Text>
+            </View>
         </View>
     )
 }
 
+const styles = StyleSheet.create({
+    item:{
+        backgroundColor:'#FFF',
+        padding: 10,
+        margin:3,
+        width:'80%',
+        borderRadius: 10,
+      },
+})
 export default Task;
